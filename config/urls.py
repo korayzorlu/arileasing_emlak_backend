c32ff547c8ba96fbdcf5e737d783b45698756c0e
korayzorlu <korayzorllu@gmail.com>
2026-08-13 14:24:14 +0300

    #260813 fix: fixed global settings

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin/clearcache/', include('clearcache.urls')),
+    
     path('api/auth/', include('accounts.urls')),
 ]
 
